8d4e3df144b756328b54c142c884332994f6652b
Nihar Patel <58643536+niharp73@users.noreply.github.com>
2021-07-27 14:42:19 -0400

    Create db.php

---
+++ login/db.php
@@ -0,0 +1,7 @@
+<?php
+
+	$conn= new mysqli("localhost","nealj_Nihar","nihar2021","nealj_Team15");
+    if (mysqli_connect_errno()){
+        echo "Failed to connect to MySQL: " . mysqli_connect_error();
+    }
+?>
